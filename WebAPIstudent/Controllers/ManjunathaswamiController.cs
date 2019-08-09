0ecd0c15dce580f5499f800f2c5e10ebc813f5dc
vinayasshetty <53181345+vinayasshetty@users.noreply.github.com>
2019-08-09 14:58:03 +0530

    Add files via upload

---
+++ WebAPIstudent/Controllers/ManjunathaswamiController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using studentlib;
+using System.Web.Http.Cors;
+namespace WebAPIstudent.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class ManjunathaswamiController : ApiController
+    {
+        //public string Getstring()
+        //{
+        //    return "hello";
+        //}
+        
+        public IEnumerable<score> Get(string usn)
+        {
+            Adoconnected dal = new Adoconnected();
+            List<score> scorelst = dal.searchscore(usn);
+            return scorelst;
+        }
+    }
+}
